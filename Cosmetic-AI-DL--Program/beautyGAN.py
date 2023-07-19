fbbdb85380097c1638582b578c23c0b34578169e
조은정 <ejjeongs@naver.com>
2023-07-19 15:22:50 +0900

    0719

--- Cosmetic-AI-DL--Program/beautyGAN.py
+++ Cosmetic-AI-DL--Program/beautyGAN.py
@@ -1,31 +1,16 @@
-# 원본 Source Code가 tensorflow 1.x 버전이므로, 저같은 경우는 2.x버전을 사용하고 있다보니
-# 원문에서 수정하여, 2.x버전에 맞추었음을 이해 바랍니다.
-
 import tensorflow.compat.v1 as tf
-# 이렇게 작성을 해주어야지, 2.x버전에서도 호환이 이루어집니다.
 import numpy as np
 import os
 import glob
 from imageio import imread, imsave
-# 'imageio'는 pip install imageio를 해주시면 설치가 됩니다.
 
 import cv2
-# OpenCV는 일반적으로, 객체, 얼굴 및 행동 인식, 독순, 모션 추적 등의 응용프로그램에서 사용이 됩니다.
-# OpenCV는 'Open Source Computer Vision'의 줄인말이며, 이미지 인식 및 분류를 위해 필수적으로 공부하시게 될 것입니다.
-
-# 받는 질문 중, 'No module named cv2'라는 에러를 많이들 접하시는 것 같기에 답변을 링크해봅니다.
-
-# cv2 같은 경우는 'pip install opencv-python'을 해주셔야 설치가 됩니다
-# 이는 OpenCV의 메인 모듈로, contrib 모듈(고급형)을 사용하고 싶으시면
-# 부수적으로 'pip install opencv-contrib-python'을 해주셔서 설치하시면 됩니다.
 
 import argparse
-# 'argparse'모듈은 도움말과 사용방법에 대한 메세지를 자동 생성하고
-# 사용자가 프로그램에 잘못된 인자를 주었을 때, 에러를 발생시킵니다.
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--no_makeup', type=str, default =
-os.path.join('C:/Users/SSAFY/Downloads', 'no_makeup', 'image.png'), help = 'path to the no_makeup image')
+os.path.join('C:/Users/SSAFY/Desktop/hi/AI-study/Cosmetic-AI-DL--Program/imgs', 'no_makeup', 'xfsy_0068.png'), help = 'path to the no_makeup image')
 args = parser.parse_args()
 
 def preprocess(img):
@@ -36,19 +21,17 @@ def deprocess(img):
 
 batch_size = 1
 img_size = 256
-no_makeup = cv2.resize(imread(args.no_makeup), (img_size, img_size))
+no_makeup = cv2.resize(cv2.imread(args.no_makeup), (img_size, img_size))
 X_img = np.expand_dims(preprocess(no_makeup), 0)
-makeups = glob.glob(os.path.join('C:/AI/BeautyGAN/imgs', 'makeup', '*.*'))
+makeups = glob.glob(os.path.join('C:/Users/SSAFY/Desktop/hi/AI-study/Cosmetic-AI-DL--Program/imgs', 'makeup', '*.*'))
 
 result = np.ones((2 * img_size, (len(makeups) + 1) * img_size, 3))
 result[img_size: 2 * img_size, :img_size] = no_makeup / 255.
 
 with tf.compat.v1.Session() as ses:
-    # ses = tf.compat.v1.Session()
     ses.run(tf.global_variables_initializer())
-    # 이미 학습한 모델을 읽어서 복원해줍니다.
-    saver = tf.train.import_meta_graph(os.path.join('C:/Users/SSAFY/Desktop/hi/Cosmetic-AI-DL--Program', 'model.meta'))
-    saver.restore(ses, tf.train.latest_checkpoint('C:/Users/SSAFY/Desktop/hi/Cosmetic-AI-DL--Program'))
+    saver = tf.train.import_meta_graph(os.path.join('C:/Users/SSAFY/Desktop/hi/AI-study/Cosmetic-AI-DL--Program/model', 'model.meta'))
+    saver.restore(ses, tf.train.latest_checkpoint('C:/Users/SSAFY/Desktop/hi/AI-study/Cosmetic-AI-DL--Program/model'))
 
     graph = tf.get_default_graph()
     X = graph.get_tensor_by_name('X:0')
@@ -65,4 +48,4 @@ with tf.compat.v1.Session() as ses:
                * img_size] = makeup / 255.
         result[img_size: 2 * img_size,
                (i + 1) * img_size: (i + 2) * img_size] = Xs_[0]
-    imsave('C:/Users/SSAFY/Downloads/result.jpg', result)
+    imsave('C:/Users/SSAFY/Desktop/hi/AI-study/Cosmetic-AI-DL--Program/result.jpg', result)
